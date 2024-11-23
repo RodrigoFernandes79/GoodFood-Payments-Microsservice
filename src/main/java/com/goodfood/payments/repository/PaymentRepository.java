6c412ba4566f53511f63fd9f4bf2007ac1a4dd7c
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-11-23 10:03:51 -0300

    fix: correct wrong import in PaymentRepository causing startup failure

--- src/main/java/com/goodfood/payments/repository/PaymentRepository.java
+++ src/main/java/com/goodfood/payments/repository/PaymentRepository.java
@@ -2,9 +2,10 @@ package com.goodfood.payments.repository;
 
 import com.goodfood.payments.model.Payment;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
+
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Page<Payment> findAll(Pageable pageable);

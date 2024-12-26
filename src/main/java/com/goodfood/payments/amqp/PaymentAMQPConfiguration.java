d6b29092bccc9602c8c35d977afa2c395416709e
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-12-26 11:15:19 -0300

    feat: configure RabbitTemplate to use Jackson2JsonMessageConverter, which converts Java objects to JSON

--- src/main/java/com/goodfood/payments/amqp/PaymentAMQPConfiguration.java
+++ src/main/java/com/goodfood/payments/amqp/PaymentAMQPConfiguration.java
@@ -1,9 +1,10 @@
 package com.goodfood.payments.amqp;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -26,5 +27,18 @@ public class PaymentAMQPConfiguration {
     public ApplicationListener<ApplicationReadyEvent> initializeAdmin(RabbitAdmin rabbitAdmin) {
         return event -> rabbitAdmin.initialize();
     }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
 
 }
